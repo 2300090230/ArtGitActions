c6311a698e465716099a131119fefbe4b76531f8
Laxman <2300090230@kluniversity.in>
2025-11-04 09:52:15 +0530

    v4.0 updated config.js

--- onlineartgallery/src/config.js
+++ onlineartgallery/src/config.js
@@ -1,8 +1,8 @@
 const config = {
     //url: "http://localhost:2030/apionlineart"
     
-   // url: "http://localhost:2025"
-     url: "/artapi"
+    url: "http://localhost:2025"
+     //url: "/artapi"
 }
 
 export default config;
